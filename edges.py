82ce2c5e12a124ced12355ba1fcc33b1a3d2cf27
root <root@localhost.localdomain>
2018-02-19 21:57:34 +0100

    edges modified

--- edges.py
+++ edges.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 cap = cv2.VideoCapture("dataset/examples-video/test.mp4")
 #ret, frame = cap.read()
 
@@ -7,8 +8,11 @@ cap = cv2.VideoCapture("dataset/examples-video/test.mp4")
 while (cap.isOpened()):
     ret, frame1 = cap.read()
     frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    ret, frame1 = cv2.threshold(frame1,0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    #ret, frame1 = cv2.threshold(frame1,0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     #frame1 = cv2.morphologyEx(frame1, cv2.MORPH_CLOSE, kernel)
+    ret, frame1 = cv2.threshold(frame1,100,255,0)
+    frame1, contours, hierarchy = cv2.findContours(frame1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
     
     
 
